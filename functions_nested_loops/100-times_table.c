44e47bbdebb6e682e2a6db87340e858306edbd68
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-05 19:21:15 -0700

    errors

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,37 +7,6 @@
  */
 void print_times_table(int n)
 {
-	int num, mult, proud;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-			
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				proud = num * mult;
-
-				if (proud <= 99)
-					_putchar(' ');
-				if (proud <= 9)
-					_putchar(' ');
-
-				if (proud >= 100)
-				{
-					_putchar((proud / 100) + '0');
-					_putchar(((proud / 10)) % 10 + '0');
-				}
-				else if (proud <= 99 && proud >= 10)
-				{
-					_putchar((proud / 10) + '0');
-				}
-				_putchar((proud % 10) + '0');
-			}
-		}
-	}
+	
+	return (0);
 }
