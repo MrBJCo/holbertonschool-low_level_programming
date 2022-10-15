86c12d741cc0a9591222ca1628d943b52d88fb07
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-14 18:47:31 -0700

    Last Pointers etc

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -14,7 +14,7 @@ char *_strpbrk(char *s, char *accept)
 	while (s[a])
 	{
 		b = 0;
-		
+
 		while (accept[b])
 		{
 			if (s[a] == accept[b])
@@ -25,7 +25,7 @@ char *_strpbrk(char *s, char *accept)
 			
 			b++;
 		}
-		
+
 		a++;
 	}
 
