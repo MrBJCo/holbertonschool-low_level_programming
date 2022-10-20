84b3e7a71fde525f5d10a90bc36648e5dbe25a94
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-20 07:20:21 -0700

    Arguments task

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
+/**
+ * main - prints its name + \n
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
 
+int main(int argc, char *argv[])   
+{
+	argc = 0;
+	
+	printf("%s\n", argv[argc]);
+	return (0);
 }
