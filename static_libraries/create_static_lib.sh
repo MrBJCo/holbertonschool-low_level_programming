ee0e4cb4bd77982baafd7d94eb14b0495a8e6a77
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-19 18:03:24 -0700

    finish

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-$-Wall -Werror -Wextra -pedantic -std=gnu89 *c.
-$gcc -c *c.
-$ar -rc libmy.a *.o
-$ranlib libholberton.a
-$nm libholberton.a
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
