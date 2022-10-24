ed14705986dbbc75f7c6ccf8f8ea62b926b7a0ab
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-24 13:28:49 +0000

    Malloc task

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *create_array - creates an array of chars, and initializes it with a specific char
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c);
+{
+	char *s;
+	unsigned int a;
+
+		if (size == 0);
+		return (NULL);
+}
