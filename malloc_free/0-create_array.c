ac8105b8ab890490e390ea47d9f915bce4c9b52c
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-24 13:57:43 +0000

    malloc task

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,19 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *s;
-	unsigned int a;
+	char *array = NULL;
+	unsigned int i;
 
-		if (size == 0)
+	if (size == 0)
 		return (NULL);
-
-		s = malloc(size * sizeof(*s));
-		if (s == NULL)
-		return (NULL);
-
-		for (a = 0; a < size; a++)
+	if (size != 0)
+	{
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
 		{
-			s[a] = c;
+			for (i = 0; i < size; i++)
+				array[i] = c;
 		}
-		return (s);
+	}
+	return (array);
 }
