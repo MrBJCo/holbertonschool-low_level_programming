e279012271fdc3142626ae0ba0f3f94da922f6b2
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-24 16:50:13 +0000

    Malloc task

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,7 +8,24 @@
  * Return: 0
  */
 
-char *_strdup(char *str)
+char *_strdup(char *str);
 {
+	int i, end;
+	char *array;
 
+	if(str = NULL)
+		return NULL;
+	for(end = 0; end <= *str; end++)
+	{
+	}
+
+	end += 1;
+	array = malloc(sizeof(char)* end);
+
+	for (i = 0; i < end; i++)
+		array[i] = str[i];
+
+	if (array == NULL)
+		return (NULL);
+	return (array);
 }
