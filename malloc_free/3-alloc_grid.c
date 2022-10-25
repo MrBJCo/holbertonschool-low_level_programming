67ac45bac6d2d8eb5e5cabe0843034de66a1cfce
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-25 14:29:24 +0000

    malloc task

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,10 +3,20 @@
 
 int **alloc_grid(int width, int height);
 {
-    int **array;
-    array = malloc(nrows * sizeof(int *));
-    for(i = 0; i < nrows; i++)
-    {
-        array[i] = malloc(ncolumns * sizeof(int));
-    }
+	int **array
+		array = malloc(nrows * sizeof(int *));
+	if(array == NULL)
+	{
+		fprintf(stderr, "out of memory\n");
+		exit or return
+	}
+	for(i = 0; i < nrows; i++)
+	{
+		array[i] = malloc(ncolumns * sizeof(int));
+		if(array[i] == NULL)
+		{
+			fprintf(stderr, "out of memory\n");
+			exit or return
+		}
+	}
 }
