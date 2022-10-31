0a4443673d1eba990c3b634de8fe66a70d70fb6c
MrBJCo <bryancarrassquillo@gmail.com>
2022-10-31 14:59:52 +0000

    struct task

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -8,17 +8,10 @@
  */
 int main(void)
 {
-#ifdef dog_t
-	printf("'dog_t' should not be a #define !\n");
-	return (0);
-#else
-	dog_t my_dog;
+	struct dog my_dog;
 
-	my_dog.name = "Ghost";
-	my_dog.age = 4.75;
-	my_dog.owner = "Jon Snow";
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
 	printf("My name is %s, I am %.2f, and my owner is %s\n",
 		my_dog.name, my_dog.age, my_dog.owner);
 	return (0);
-#endif
 }
