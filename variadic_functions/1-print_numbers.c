7f7850d5fb97f0ed8185c426bcbbf692b777026c
MrBJCo <bryancarrassquillo@gmail.com>
2022-11-02 16:02:50 +0000

    variatic task

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,53 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
-  * print_numbers - ...
-  * @separator: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * print_numbers - prints a number
+ * @separator: is the string to be printed between numbers
+ * @n: number of interger passed to the functions
+ * Return: if seperator is null dont print
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args; /** args will show the list */
-	unsigned int i = 0; /** the const will indicate for n */
+	va_list p;
+	unsigned int i;
 
-	if (n > 0) /** n is better that 0 */
+	if (separator == NULL)
 	{
-		va_start(args, n); 
-		/**
-		 * args and n will interact integer
-		 */
-
-		while (i < n) 
-			/** i is not better that n */ 
-		{
-			printf("%d", va_arg(args, int)); 
-			/**
-			 *  show the va_arg and int to the output
-			 */
-
-			if (i != n - 1  && separator != NULL) // i will not be better to n
-				/**
-				 * And will show the direction of the separator
-				 * but will not be true and can't return NUll
-				 */
-				printf("%s", separator);
-
-			i++; 
-			/**
-			 * i will increase the value
-			 */
-		}
-		va_end(args); 
-		/**
-		 * will leave or return to regular statement
-		 */
 	}
-	printf("\n"); 
-	/** 
-	 * will add white space so will not see it weird 
-	 */
+
+	va_start(p, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(p, int));
+		if (separator && i != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(p);
 }
