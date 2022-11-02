c6b3dcb6c6934ca744ba31f171cdc6969cd2ccb7
MrBJCo <bryancarrassquillo@gmail.com>
2022-11-02 13:58:30 +0000

    variatic task

---
+++ variadic_fuctions/0-sum_them_all.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - fuction that return the sum of all its parameters.
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	int n;
+	int sum;
+
+}
